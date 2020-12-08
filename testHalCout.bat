4929ad006afb0534831017457f76422af6fe43b9
Hau <haugillesche@gmail.com>
2020-12-07 22:11:21 -0500

    Task 3 and 4 test files and scripts

--- testHalCout.bat
+++ testHalCout.bat
@@ -1,4 +1,3 @@
-@if exist "tout.o" del "tout.o"
 @if exist "tout.hex" del "tout.hex" 
 avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p tout.c hal.c hal_Out.c bsl_COut.c bsl_xtoa.c -o tout.o
 avr-objcopy -O ihex -j .text -j .data tout.o  tout.hex
